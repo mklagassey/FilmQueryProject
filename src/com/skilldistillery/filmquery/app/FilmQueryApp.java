54789e2b4bce4a6c337355766a66544a7ba978a8
mklagassey <mklagassey@gmail.com>
2021-06-26 15:51:56 -0400

    updated film output formatting

--- src/com/skilldistillery/filmquery/app/FilmQueryApp.java
+++ src/com/skilldistillery/filmquery/app/FilmQueryApp.java
@@ -40,61 +40,58 @@ public class FilmQueryApp {
 
 	private void startUserInterface(Scanner input) throws SQLException {
 		boolean keepGoing = true;
+
 		do {
-			
-		int userChoice;
-		System.out.println("Please choose an option: \n" + "1) Look up film by id\n"
-				+ "2) Look up film by keyword search\n" + "3) Exit");
+			int userChoice;
+			System.out.println();
+			System.out.println("Please choose an option: \n" + "1) Look up film by id\n"
+					+ "2) Look up film by keyword search\n" + "3) Exit");
 
-		userChoice = input.nextInt();
+			userChoice = input.nextInt();
 
-		switch (userChoice) {
-		case 1:
-			System.out.println("Please enter film's unique ID # -----");
-			int idNum = input.nextInt();
-			Film film = db.findFilmById(idNum);
-			displayFilm(film);
-			break;
-		case 2:
-			System.out.println("Please enter your search criteria ------");
-			String searchFor = input.next();
-			List<Film> foundFilms = db.findFilmsBySearchString(searchFor);
-			for (Film film2 : foundFilms) {
-				displayFilm(film2);
+			switch (userChoice) {
+			case 1:
+				System.out.println("Please enter film's unique ID # -----");
+				int idNum = input.nextInt();
+				Film film = db.findFilmById(idNum);
+				displayFilm(film);
+				break;
+			case 2:
+				System.out.println("Please enter your search criteria ------");
+				String searchFor = input.next();
+				List<Film> foundFilms = db.findFilmsBySearchString(searchFor);
+				for (Film film2 : foundFilms) {
+					displayFilm(film2);
+				}
+				break;
+			case 3:
+				System.out.println("Thanks for using CIN-O-MATIC, come back soon!");
+				keepGoing = false;
+				break;
+
+			default:
+				System.out.println("Sorry, try again. That number is invalid.");
+				break;
 			}
-			break;
-		case 3:
-			System.out.println("Thanks for using CIN-O-MATIC, come back soon!");
-			keepGoing = false;
-			break;
-
-		default:
-			System.out.println("Sorry, try again. That number is invalid.");
-			break;
-		}
 		} while (keepGoing);
 
 	}
-	
 
 	private void displayFilm(Film f) {
+		System.out.println();
 		if (f != null) {
-			System.out.println("TITLE: " + f.getTitle() + " " + 
-					"LANGUAGE: " + f.getLanguageName()+ " " +
-					"RELEASED: " + f.getReleaseYear() + " " + 
-					"RATED: " + f.getRating() + "\n " + 
-					"----> DESCRIPTION: " + f.getDescription()
-					);
+			System.out.println("TITLE: " + f.getTitle() + "\n" + "LANGUAGE: " + f.getLanguageName() + " " + "RELEASED: "
+					+ f.getReleaseYear() + " " + "RATED: " + f.getRating() + "\n" + "DESCRIPTION:\n "
+					+ f.getDescription());
 			System.out.println("CAST: ");
-			for (int i = 0 ; i < f.getActorList().size() ; i++) {
+			for (int i = 0; i < f.getActorList().size(); i++) {
 				Actor actor = f.getActorList().get(i);
 				if (i < f.getActorList().size() - 1) {
-					System.out.print(actor.getFirstName() + " " + actor.getLastName() + ", ");
+					System.out.print(" " + actor.getFirstName() + " " + actor.getLastName() + ",");
 				} else {
-					System.out.print(actor.getFirstName() + " " + actor.getLastName());
+					System.out.print(" " + actor.getFirstName() + " " + actor.getLastName());
 					System.out.println();
 				}
-//				System.out.println();
 			}
 		} else {
 			System.out.println("Sorry, that film does not appear in our database. Please try again.");
